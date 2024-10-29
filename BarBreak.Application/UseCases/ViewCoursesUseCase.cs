f7e52a51134c49da39832577f7caa0cc5aa55aa8
OlexiyITF <OlexiyITF@gmail.com>
2024-10-29 18:01:17 +0200

    Add Validator

--- BarBreak.Application/UseCases/ViewCoursesUseCase.cs
+++ BarBreak.Application/UseCases/ViewCoursesUseCase.cs
@@ -1,44 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
-using System.Text;
 using BarBreak.Core.Repositories;
 using BarBreak.Core.DTOs;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using BarBreak.Application.Validators;
 using BarBreak.Core.Entities;
-
 
 namespace BarBreak.Application.UseCases
 {
     public class ViewCoursesUseCase
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly ViewCoursesValidator _validator;
 
         public ViewCoursesUseCase(ICourseRepository courseRepository)
         {
             _courseRepository = courseRepository;
+            _validator = new ViewCoursesValidator();
         }
 
         public async Task<List<CourseDto>> ExecuteAsync(int userId)
         {
+            // Виконуємо валідацію userId
+            var validationResult = _validator.Validate(userId);
+            if (!validationResult.IsValid)
+            {
+                // Повертаємо порожній список, якщо валідація не пройшла
+                return new List<CourseDto>();
+            }
+
             // Отримуємо курси з репозиторію
             var courses = await _courseRepository.GetCoursesForUserAsync(userId);
 
             // Мапимо `Course` на `CourseDto`
-            var courseDtos = new List<CourseDto>();
-            foreach (var course in courses)
+            var courseDtos = courses.Select(course => new CourseDto
             {
-                courseDtos.Add(new CourseDto
-                {
-                    Id = course.Id,
-                    Title = course.Title,
-                    Description = course.Description
-                });
-            }
+                Id = course.Id,
+                Title = course.Title,
+                Description = course.Description
+            }).ToList();
 
             return courseDtos;
         }
     }
 }
-
---
+++ BarBreak.Application/Validators/ViewCoursesValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace BarBreak.Application.Validators
+{
+    public class ViewCoursesValidator : AbstractValidator<int>
+    {
+        public ViewCoursesValidator()
+        {
+            RuleFor(userId => userId)
+                .GreaterThan(0)
+                .WithMessage("User ID must be greater than zero.");
+        }
+    }
+}
+
