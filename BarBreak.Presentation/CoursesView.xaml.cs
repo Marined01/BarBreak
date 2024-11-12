86e462d8a609f94291353209ccc08d4910d61072
OlexiyITF <OlexiyITF@gmail.com>
2024-11-12 05:08:24 +0200

    created UI for ViewCoursesUseCase

---
+++ BarBreak.Presentation/CoursesView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using BarBreak.Presentation.ViewModels;
+
+namespace BarBreak.Presentation
+{
+    public partial class CoursesView : Window
+    {
+        public CoursesView()
+        {
+            InitializeComponent();
+            this.DataContext = new CoursesViewModel();
+        }
+    }
+}
---
+++ BarBreak.Presentation/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace BarBreak.Presentation.ViewModels
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; } // Наприклад, URL або шлях до зображення
+        public bool IsFavorite { get; set; }
+    }
+
+    public class CoursesViewModel
+    {
+        public ObservableCollection<Course> Courses { get; set; }
+
+        public CoursesViewModel()
+        {
+            // Заповнення списку курсів тестовими даними
+            Courses = new ObservableCollection<Course>
+            {
+                new Course { Name = "Course 1", Description = "Description for Course 1", Image = "image1.png", IsFavorite = true },
+                new Course { Name = "Course 2", Description = "Description for Course 2", Image = "image2.png", IsFavorite = false },
+                // Додати інші курси за потребою
+            };
+        }
+    }
+}
