4d92cac07bbf20f82c704152df4cc8b4aa402765
Mariana <mariana.hayovska@gmail.com>
2024-11-26 05:08:17 +0200

    add usecase from Ihor to this project, add RegisterGuestWindow

---
+++ BarBreak.Presentation_/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BarBreak.Presentation_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
